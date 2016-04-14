b1e538ff67727b58831ac966411eba5ff5d061a1
Dennis H <dennis.heimbert@gmail.com>
2016-04-14 15:34:03 +0200

    hej plugin

---
+++ plugins/hej.rb
@@ -0,0 +1,11 @@
+require 'cinch'
+
+class Hej
+  include Cinch::Plugin
+
+  match /.*(?=.*h)(?=.*e)(?=.*j)(?!.*[a-dfgik-z]).*[!.]?/i, use_prefix: false
+
+  def execute(m)
+    m.reply "Hallå hej, #{m.user.nick}!"
+  end
+end
