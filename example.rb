b705a3d23d7ee76e515373fa78a16f9be5dddccd
snapcase <snapcase@octalzeroes.com>
2016-11-15 11:09:20 +0100

    fix example bot

--- example.rb
+++ example.rb
@@ -7,20 +7,20 @@ module Clickbait
   # bot
   bot = Cinch::Bot.new do
     configure do |c|
-      c.nick = "mybot"
-      c.user = "something"
-      c.server = "irc.freenode.net"
-      c.channels = ["#chan1", "#chan2"]
+      c.nick = 'mybot'
+      c.user = 'something'
+      c.server = 'irc.freenode.net'
+      c.channels = ['#chan1', '#chan2']
       c.port = 7000
       c.ssl.use = true
       c.ssl.verify = true
       c.plugins.plugins = Plugins.constants.map { |p| Plugins.const_get p }
       c.plugins.options[Plugins::Youtube] = { api_key: 'CHANGE_ME' }
-      c.plugins.options[Plugins::HTTPTitle] = { blacklist: [ 
-        "youtube.com",
-        "youtu.be",
-        "twitch.tv",
-        "imdb.com"
+      c.plugins.options[Plugins::HTTPTitle] = { blacklist: [
+        'youtube.com',
+        'youtu.be',
+        'twitch.tv',
+        'imdb.com'
       ] }
     end
   end
